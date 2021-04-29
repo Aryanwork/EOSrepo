3bd39cce2a2aea67321f8e997194a8c1999af6ba
Ryan <taher.aryannejad@gmail.com>
2021-04-29 18:39:31 -0400

    Dev2

--- src/sigin/test1.java
+++ src/sigin/test1.java
@@ -5,6 +5,10 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("OP1");
+System.out.println("OP6");
+System.out.println("OP7");
+System.out.println("OP8");
+System.out.println("OP9");
 	}
 
 }
--- src/signout/test2.java
+++ src/signout/test2.java
@@ -5,6 +5,9 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("OP2");
+System.out.println("OP3");
+System.out.println("OP4");
+System.out.println("OP5");
 	}
 
 }
